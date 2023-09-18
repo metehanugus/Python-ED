4a978e94f36cc071306287dfc5cd1944022caedc
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-09-18 18:04:10 +0300

    workshop
    workshoplar eklendi

---
+++ Workshops/1_sayilarinYeriniDegistir.py
@@ -0,0 +1,8 @@
+x = 10
+y = 20
+
+a = x
+x = y
+y = a
+
+print(x, y)
---
+++ Workshops/2_kacMileEder.py
@@ -0,0 +1,7 @@
+
+donusum = 0.621371192
+km = float(input("Çevirmek istediğiniz kilometre? "))
+
+mil = km * donusum
+
+print(str(km) + " Km = " + str(mil)+" mil eder.")
