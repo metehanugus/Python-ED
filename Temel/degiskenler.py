ffd5547830a8664434f215f4b20d163e1f53f3a1
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-09-18 17:15:59 +0300

    Create degiskenler.py

---
+++ Temel/degiskenler.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 17:08:15 2023
+
+@author: meteh
+"""
+
+a = 10 #integer
+b = 'x' #string /char
+c = 10.5 #float
+d = "metehan"
+f = "541541541"
+
+print(float(f) + 25)
+print(type(f))
+print(type(c))
+print(type(b))
